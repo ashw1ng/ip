fe0605920994aef63539ca7a8454c2ff2364a335
ashw1ng <ashwingovind2004@gmail.com>
2026-01-23 12:44:20 +0800

    level-0

---
+++ src/main/java/UrMum.java
@@ -0,0 +1,13 @@
+public class UrMum {
+    public static void main(String[] args) {
+        String line = "____________________________________________________________";
+        String greeting = " Hello! I'm UrMum\n What can I do for you?";
+        String farewell = " Bye. Hope to see you again soon!";
+
+        System.out.println(line);
+        System.out.println(greeting);
+        System.out.println(line);
+        System.out.println(farewell);
+        System.out.println(line);        
+    }
+}
