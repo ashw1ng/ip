8c2a95bcaef7d349aa45bb3ae0c607bcffc2306f
ashw1ng <ashwingovind2004@gmail.com>
2026-01-23 13:08:24 +0800

    refined

--- src/main/java/UrMum.java
+++ src/main/java/UrMum.java
@@ -1,27 +1,21 @@
+import java.util.Scanner;
+
 public class UrMum {
     public static void main(String[] args) {
-        String line = "____________________________________________________________";
-        String greeting = " Hello! I'm UrMum\n What can I do for you?";
-        String farewell = " Bye. Hope to see you again soon!";
+        //String line = "____________________________________________________________";
+        String welcome = " Hello! I'm UrMum\n What can I do for you?";
+        String goodbye = " Bye. Hope to see you again soon!";
 
-        System.out.println(line);
-        System.out.println(greeting);
-        System.out.println(line);
-        System.out.println(farewell);
-        System.out.println(line);
+        System.out.println(welcome);
 
         Scanner scanner = new Scanner(System.in);
         while (true) {
             String input = scanner.nextLine();
             if (input.equals("bye")) {
-                System.out.println(line);
-                System.out.println(farewell);
-                System.out.println(line);
+                System.out.println(goodbye);
                 break;
             } else {
-                System.out.println(line);
                 System.out.println(" " + input);
-                System.out.println(line);
             }
         }
         scanner.close();        
