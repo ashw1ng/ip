790d31a2c37c98d82af6c5d41b87804c5d912c4f
ashw1ng <ashwingovind2004@gmail.com>
2026-01-23 13:32:15 +0800

    level-3

--- src/main/java/UrMum.java
+++ src/main/java/UrMum.java
@@ -2,15 +2,14 @@ import java.util.Scanner;
 
 public class UrMum {
     public static void main(String[] args) {
-        //String line = "____________________________________________________________";
         String welcome = " Hello! I'm UrMum\n What can I do for you?";
         String goodbye = " Bye. Hope to see you again soon!";
 
         System.out.println(welcome);
 
         Scanner scanner = new Scanner(System.in);
-        String[] messages = new String[100];
-        int msgCount = 0;
+        Task[] tasks = new Task[100];
+        int taskCount = 0;
 
         while (true) {
             String input = scanner.nextLine();
@@ -18,12 +17,39 @@ public class UrMum {
                 System.out.println(goodbye);
                 break;
             } else if (input.equals("list")) {
-                for (int i = 0; i < msgCount; i++) {
-                    System.out.println((i + 1) + ". " + messages[i]);
+                System.out.println(" Here are the tasks in your list:");
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println(" " + (i + 1) + "." + tasks[i]);
+                }
+            } else if (input.startsWith("mark ")) {
+                try {
+                    int idx = Integer.parseInt(input.substring(5)) - 1;
+                    if (idx >= 0 && idx < taskCount) {
+                        tasks[idx].markAsDone();
+                        System.out.println(" Nice! I've marked this task as done:");
+                        System.out.println("   " + tasks[idx]);
+                    } else {
+                        System.out.println(" Invalid task number.");
+                    }
+                } catch (NumberFormatException e) {
+                    System.out.println(" Please enter a valid task number.");
+                }
+            } else if (input.startsWith("unmark ")) {
+                try {
+                    int idx = Integer.parseInt(input.substring(7)) - 1;
+                    if (idx >= 0 && idx < taskCount) {
+                        tasks[idx].markAsNotDone();
+                        System.out.println(" OK, I've marked this task as not done yet:");
+                        System.out.println("   " + tasks[idx]);
+                    } else {
+                        System.out.println(" Invalid task number.");
+                    }
+                } catch (NumberFormatException e) {
+                    System.out.println(" Please enter a valid task number.");
                 }
             } else {
-                messages[msgCount] = input;
-                msgCount++;
+                tasks[taskCount] = input;
+                taskCount++;
                 System.out.println("added: " + input);
             }
         }
